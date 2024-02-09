c5ca5aebf565cf923fe0cbf3c9ea5707b51800cc
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-09 17:57:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -338,8 +338,8 @@ if (file_exists('/etc/dstar-radio.mmdvmhost')) {
 
 <div class="footer">
 Pi-Star / Pi-Star Dashboard, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
-ircDDBGateway Dashboard by Hans-J. Barthen (EA3EIZDD),<br />
-MMDVMDash developed by Kim Huebel (EA3EIZZZZZZZ), <br />
+ircDDBGateway Dashboard by Hans-J. Barthen (DL5DI),<br />
+MMDVMDash developed by Kim Huebel (DG9VH), <br />
 Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistarusergroup/" target="_new">here for the Facebook Group</a><br />
 or Click <a style="color: #ffffff;" href="https://forum.pistar.uk/" target="_new">here to join the Support Forum</a><br />	
 Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.pistar.uk/downloads/" target="_new">here</a>.<br />
