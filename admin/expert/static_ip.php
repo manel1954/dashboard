53c99222ac26adf69dba6f50450593e1f1f38cd0
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-09 23:56:49 +0100

    Create static_ip.php

---
+++ admin/expert/static_ip.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+$indicativo=strtoupper($_POST["indicativo"]);
+$_SESSION["indicativo"]=$indicativo;
+
+//$indicativo = substr("$indicativo", 9, 11);
+
+exec("sudo sed -i '1c $indicativo' /etc/dhcpcd.conf");
+
+
+
+
+
+
+header("Location:index.php");	
+
+?>
