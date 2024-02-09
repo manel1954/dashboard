af69713c20afc8c967f493824664fe88da983dd2
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-09 23:33:54 +0100

    Update cambia_configuracion_indicativo.php

--- admin/expert/cambia_configuracion_indicativo.php
+++ admin/expert/cambia_configuracion_indicativo.php
@@ -3,6 +3,8 @@ session_start();
 $indicativo=strtoupper($_POST["indicativo"]);
 $_SESSION["indicativo"]=$indicativo;
 
+$indicativo = substr("$indicativo", 9, 11);
+
 exec("sudo sed -i '1c Callsign=$indicativo' /etc/dhcpcd.conf");
 
 
