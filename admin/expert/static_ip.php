8d644971ffb9ed3a643616f155ab33b899496b57
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-10 00:37:37 +0100

    Update static_ip.php

--- admin/expert/static_ip.php
+++ admin/expert/static_ip.php
@@ -8,7 +8,7 @@ $ip=($_POST["static_ip"]);
 //$_SESSION["static_ip"]=$static_ip;
 
 $static_ip="static ip_address=" . $ip . "/24";
-
+$dns="static domain_name_servers=" . $ip . " 8.8.8.8";
 
 //esta linea escribe simplemente interface eth0
 exec("sudo sed -i '44c interface eth0' /etc/dhcpcd.conf");
@@ -17,6 +17,9 @@ exec("sudo sed -i '45c $static_ip' /etc/dhcpcd.conf");
 
 //esta linea escribe simplemente static router
 exec("sudo sed -i '47c static routers=$ip' /etc/dhcpcd.conf");
+
+//esta linea escribe simplemente ip + dns
+exec("sudo sed -i '48c static routers=$dns' /etc/dhcpcd.conf");
 
 header("Location:index.php");	
 ?>
