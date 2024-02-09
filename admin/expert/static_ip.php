df87bfeae020f01c95aac2de63b26ece26719fb8
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-10 00:01:34 +0100

    Update static_ip.php

--- admin/expert/static_ip.php
+++ admin/expert/static_ip.php
@@ -1,17 +1,7 @@
 <?php 
 session_start();
-$indicativo=strtoupper($_POST["indicativo"]);
-$_SESSION["indicativo"]=$indicativo;
-
-//$indicativo = substr("$indicativo", 9, 11);
-
-exec("sudo sed -i '1c $indicativo' /etc/dhcpcd.conf");
-
-
-
-
-
-
+$static_ip=strtoupper($_POST["static_ip"]);
+$_SESSION["static_ip"]=$static_ip;
+exec("sudo sed -i '45c $static_ip' /etc/dhcpcd.conf");
 header("Location:index.php");	
-
 ?>
