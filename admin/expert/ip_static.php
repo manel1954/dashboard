0fa90c42b24bc9a4deb632a41e3405570c0b2408
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-02-09 22:06:25 +0100

    Update ip_static.php

--- admin/expert/ip_static.php
+++ admin/expert/ip_static.php
@@ -1,4 +1,31 @@
 <?php 
-exec("sudo sh ip_static.sh");
-header("Location: index.php");
+//exec("sudo sh ip_static.sh");
+//header("Location: index.php");
  ?>
+ <?php
+$url_script = 'ip_static.sh';
+
+// Descarga el script desde la web
+$script_content = file_get_contents($url_script);
+
+if ($script_content !== false) {
+    // Crea un archivo temporal para almacenar el script
+    $temp_file = tempnam(sys_get_temp_dir(), 'script');
+    file_put_contents($temp_file, $script_content);
+
+    // Otorga permisos de ejecución al archivo
+    chmod($temp_file, 0755);
+
+    // Ejecuta el script
+    $output = shell_exec($temp_file);
+
+    // Muestra la salida (puedes hacer otras cosas con la salida según tus necesidades)
+    echo "Salida del script:\n";
+    echo $output;
+
+    // Elimina el archivo temporal
+    unlink($temp_file);
+} else {
+    echo "Error al descargar el script.";
+}
+?>
